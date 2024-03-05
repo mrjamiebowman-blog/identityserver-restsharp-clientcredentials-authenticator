a13d7e7693cbac388f4c3dd60a54fa04c139a38e
Jamie Bowman <mrjamiebowman@gmail.com>
2024-03-05 12:14:01 -0600

    read console for T command

--- src/MrJB.IdentityServer.RestSharp.Console/Program.cs
+++ src/MrJB.IdentityServer.RestSharp.Console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// console
+Console.WriteLine("Press [T] to Test.");
+ConsoleKeyInfo keyInfo;
+
+do
+{
+    // read key
+    keyInfo = Console.ReadKey();
+
+    if (keyInfo.Key == ConsoleKey.T) {
+        Console.WriteLine("Running Auth Test...");
+        break;
+    }
+} while (true);
