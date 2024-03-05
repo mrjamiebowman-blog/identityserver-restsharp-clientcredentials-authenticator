619a68b82f9479c2896e66000c861eb78f364361
Jamie Bowman <mrjamiebowman@gmail.com>
2024-03-05 11:59:12 -0600

    initial set up of identityserver

--- src/MrJB.IdentityServer.RestSharp.Console/Configuration/ApiClientConfiguration.cs
+++ src/MrJB.IdentityServer.RestSharp.Console/Configuration/ApiClientConfiguration.cs
@@ -4,14 +4,14 @@ public class ApiClientConfiguration
 {
     public const string Position = "ApiClient";
 
-    public string IdentityServerUrl { get; set; } = "https://localhost:5000";
+    public string IdentityServerUrl { get; set; } = "http://localhost:5000";
 
-    public string ClientId { get; set; }
+    public string ClientId { get; set; } = "m2m.client";
 
-    public string ClientSecret { get; set; }
+    public string ClientSecret { get; set; } = "511536EF-F270-4058-80CA-1C89C192F69A";
 
     /// <summary>
     ///  This API does not exist.
     /// </summary>
-    public string ApiUrl { get; set; } = "https:/test.mrjamiebowman.com/api/";
+    public string ApiUrl { get; set; } = "http://localhost:5244/";
 }
