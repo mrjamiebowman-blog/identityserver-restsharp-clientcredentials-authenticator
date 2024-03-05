ca82604bc09078d825b65cd76119342913ef5dbd
Jamie Bowman <mrjamiebowman@gmail.com>
2024-03-05 12:19:41 -0600

    serilog

--- src/MrJB.IdentityServer.RestSharp.Console/Program.cs
+++ src/MrJB.IdentityServer.RestSharp.Console/Program.cs
@@ -1,5 +1,21 @@
-﻿// console
-Console.WriteLine("Press [T] to Test.");
+﻿using MrJB.IdentityServer.RestSharp.Console.Configuration;
+using MrJB.IdentityServer.RestSharp.Console.Services;
+using Serilog;
+
+// logging
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+// configuration
+ApiClientConfiguration apiClientConfiguration = new ApiClientConfiguration();
+
+// services
+var apiClientService = new ApiClientService(apiClientConfiguration);
+
+Log.Information("Press [T] to Test.");
 ConsoleKeyInfo keyInfo;
 
 do
@@ -8,7 +24,7 @@ do
     keyInfo = Console.ReadKey();
 
     if (keyInfo.Key == ConsoleKey.T) {
-        Console.WriteLine("Running Auth Test...");
+        Log.Information("Running Auth Test...");
         break;
     }
 } while (true);
--- src/MrJB.IdentityServer.RestSharp.Console/Services/ApiClientService.cs
+++ src/MrJB.IdentityServer.RestSharp.Console/Services/ApiClientService.cs
@@ -1,6 +1,18 @@
-﻿namespace MrJB.IdentityServer.RestSharp.Console.Services;
+﻿using MrJB.IdentityServer.RestSharp.Console.Configuration;
+
+namespace MrJB.IdentityServer.RestSharp.Console.Services;
 
 public class ApiClientService : IApiClientService
 {
+    // logging
 
+    // services
+
+    // configuration
+    private readonly ApiClientConfiguration _apiClientConfiguration;
+
+    public ApiClientService(ApiClientConfiguration apiClientConfiguration)
+    {
+        _apiClientConfiguration = apiClientConfiguration;
+    }
 }
