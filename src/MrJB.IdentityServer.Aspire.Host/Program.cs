31767e184848a63d4c84ce00ec58ff51775d0883
Jamie Bowman <mrjamiebowman@gmail.com>
2024-03-06 13:06:15 -0600

    initial set up

--- src/MrJB.IdentityServer.Aspire.Host/Program.cs
+++ src/MrJB.IdentityServer.Aspire.Host/Program.cs
@@ -1,3 +1,10 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// components
+
+// services
+builder.AddProject<Projects.MrJB_IdentityServer>("identity-server");
+builder.AddProject<Projects.MrJB_IdentityServer_Api>("demo-api");
+builder.AddProject<Projects.MrJB_IdentityServer_RestSharp_Console>("console-app");
+
 builder.Build().Run();
